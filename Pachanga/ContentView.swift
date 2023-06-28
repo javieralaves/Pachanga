905d57c1b039b7cad3d7087362fc8f9d4dee5e0a
Javier Alaves <jayalaves@gmail.com>
2023-06-28 22:01:52 +0200

    Xcode to GitHub test changes

--- Pachanga/ContentView.swift
+++ Pachanga/ContentView.swift
@@ -10,10 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "volleyball")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, Muchavista!")
+                .padding(.bottom, 4)
+            Text("Hello, Muchavista squad!")
         }
         .padding()
     }
