f811c44dd26831b0228300c97225621ce731de39
Javier Alaves <javieralaves@me.com>
2023-06-28 21:48:58 +0200

    Initial Commit

---
+++ Pachanga/PachangaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PachangaApp.swift
+//  Pachanga
+//
+//  Created by Javier Alaves on 28/6/23.
+//
+
+import SwiftUI
+
+@main
+struct PachangaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
