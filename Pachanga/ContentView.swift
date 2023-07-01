a4b2f7ff03443641b82247785e4b9c5454546150
Javier Alaves <jayalaves@gmail.com>
2023-07-01 11:39:04 +0200

    Content view update

--- Pachanga/ContentView.swift
+++ Pachanga/ContentView.swift
@@ -9,35 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     
-    var playerOne = DataService().getPlayers()[0]
-    var playerTwo = DataService().getPlayers()[1]
-    var playerThree = DataService().getPlayers()[2]
-    var playerFour = DataService().getPlayers()[3]
-    
-    // Not able to fetch players from this, will try later
-    var nextMatch = DataService().getPachangas()[0]
-    
     var body: some View {
-        VStack (alignment: .leading) {
-            Text("Próxima pachanga")
-                .font(.headline)
-            
-            // Should already fill this with a Match instance instead of individual player instances
-            ZStack {
-                RoundedRectangle(cornerRadius: 8, style: .continuous)
-                    .frame(height: 120)
-                    .foregroundColor(.gray.opacity(0.20))
-                VStack {
-                    Text("\(playerOne.firstName) y \(playerTwo.firstName)")
-                    Text("vs")
-                    Text("\(playerThree.firstName) y \(playerFour.firstName)")
+        NavigationView {
+            Form {
+                Section {
+                    NavigationLink {
+                        NewMatch()
+                    } label: {
+                        Text("Nueva pachanga")
+                    }
+                }
+                Section {
+                    Text("No hay pachangas")
                 }
             }
-            Spacer()
         }
-        .padding()
     }
-    
 }
 
 struct ContentView_Previews: PreviewProvider {
