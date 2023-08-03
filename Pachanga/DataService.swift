b6d30a8feb5d37d37217e3646cfbe315f70be4a1
Javier Alaves <jayalaves@gmail.com>
2023-08-03 17:39:50 +0200

    Clean up data service with sample players array

--- Pachanga/DataService.swift
+++ Pachanga/DataService.swift
@@ -10,10 +10,7 @@ import Foundation
 struct DataService {
     
     func getPlayers() -> [Player] {
-        return [Player(firstName: "Javier", lastName: "Alaves", profileImage: "javi"),
-                Player(firstName: "Jorge", lastName: "Yoldi", profileImage: "jorge"),
-                Player(firstName: "Diego", lastName: "Cortes", profileImage: "diego"),
-                Player(firstName: "Alvaro", lastName: "Perez", profileImage: "alvaro")]
+        return Player.samplePlayers
     }
     
     func getPachangas() -> [Match] {
