d792499b69f838be49fd284278365a90fd71a7c1
Javier Alaves <jayalaves@gmail.com>
2023-08-14 22:05:37 +0200

    session struct with codable conformance

--- Pachanga/Core/Firestore/SessionManager.swift
+++ Pachanga/Core/Firestore/SessionManager.swift
@@ -11,4 +11,67 @@ import Foundation
 
 struct Session: Codable {
     
+    // props
+    let sessionId: String
+    let dateCreated: Date
+    var location: String
+    var sessionDate: Date
+    var players: [DBUser] = []
+    var isBallAvailable: Bool = false
+    var areLinesAvailable: Bool = false
+    
+    // custom init
+    init(
+        sessionId: String,
+        dateCreated: Date,
+        location: String,
+        sessionDate: Date,
+        players: [DBUser],
+        isBallAvailable: Bool,
+        areLinesAvailable: Bool
+    ) {
+        self.sessionId = sessionId
+        self.dateCreated = dateCreated
+        self.location = location
+        self.sessionDate = dateCreated
+        self.players = players
+        self.isBallAvailable = isBallAvailable
+        self.areLinesAvailable = areLinesAvailable
+    }
+    
+    // custom coding strategy
+    enum CodingKeys: String, CodingKey {
+        case sessionId = "session_id"
+        case dateCreated = "date_created"
+        case location = "location"
+        case sessionDate = "session_date"
+        case players = "players"
+        case isBallAvailable = "is_ball_available"
+        case areLinesAvailable = "are_lines_available"
+    }
+    
+    // to load a session from db with decoder
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.sessionId = try container.decode(String.self, forKey: .sessionId)
+        self.dateCreated = try container.decode(Date.self, forKey: .dateCreated)
+        self.location = try container.decode(String.self, forKey: .location)
+        self.sessionDate = try container.decode(Date.self, forKey: .sessionDate)
+        self.players = try container.decode([DBUser].self, forKey: .players)
+        self.isBallAvailable = try container.decode(Bool.self, forKey: .isBallAvailable)
+        self.areLinesAvailable = try container.decode(Bool.self, forKey: .areLinesAvailable)
+    }
+
+    // to encode session instance into db with encoder
+    func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(self.sessionId, forKey: .sessionId)
+        try container.encode(self.dateCreated, forKey: .dateCreated)
+        try container.encode(self.location, forKey: .location)
+        try container.encode(self.sessionDate, forKey: .sessionDate)
+        try container.encode(self.players, forKey: .players)
+        try container.encode(self.isBallAvailable, forKey: .isBallAvailable)
+        try container.encode(self.areLinesAvailable, forKey: .areLinesAvailable)
+    }
+    
 }
