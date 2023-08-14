9ead6eae091789a795ebf07e195067830ebb273e
Javier Alaves <jayalaves@gmail.com>
2023-08-14 21:02:43 +0200

    deprecate old files

---
+++ Pachanga/Core/Firestore/SessionManager.swift
@@ -0,0 +1,14 @@
+//
+//  SessionManager.swift
+//  Pachanga
+//
+//  Created by Javier Alaves on 14/8/23.
+//
+
+import FirebaseFirestore
+import FirebaseFirestoreSwift
+import Foundation
+
+struct Session: Codable {
+    
+}
