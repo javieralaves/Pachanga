3485a5ded21557e152f415d57bbbf4b1cd50ba29
Ignacio Alavés <nachoalasanchez@gmail.com>
2023-08-22 13:50:10 +0200

    Fixed bugs where save and edit buttons wouldn't dismiss and also update Session info after being edited

--- Pachanga/Core/Sessions/NewSession.swift
+++ Pachanga/Core/Sessions/NewSession.swift
@@ -66,9 +66,7 @@ struct NewSession: View {
                         print(error)
                     }
                 }
-                
-                // dismiss()
-                
+                dismiss()
             }
         }
         
