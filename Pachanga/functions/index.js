bc3f77b52dbf2e82076a2486a77193cd4b69abb4
Javier Alaves <jayalaves@gmail.com>
2023-09-19 12:28:09 +0200

    index.js functions update

--- Pachanga/functions/index.js
+++ Pachanga/functions/index.js
@@ -2,6 +2,43 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
+async function getUsersInSession(sessionId) {
+  const sessionRef = admin.firestore().collection("sessions").doc(sessionId);
+  const sessionDoc = await sessionRef.get();
+
+  if (!sessionDoc.exists) {
+    console.log("No such session!");
+    return [];
+  }
+
+  return sessionDoc.data().userIds || [];
+}
+
+async function sendNotificationToUsers(userIds, sessionData) {
+  // Fetch tokens of the users
+  const tokens = [];
+  for (const userId of userIds) {
+    const userRef = admin.firestore().collection("users").doc(userId);
+    const userDoc = await userRef.get();
+
+    if (userDoc.exists && userDoc.data().token) {
+      tokens.push(userDoc.data().token);
+    }
+  }
+
+  // Prepare the message
+  const message = {
+    notification: {
+      title: "Session Update",
+      body: `Someone joined or left the session: ${sessionData.name}`,
+    },
+    tokens: tokens,
+  };
+
+  // Send the message
+  await admin.messaging().sendMulticast(message);
+}
+
 exports.notifySessionChange = functions.firestore
   .document("sessions/{sessionId}")
   .onWrite(async (change, context) => {
