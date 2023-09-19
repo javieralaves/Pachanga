57218ac7efc06db8539741277cc3a49815aaff1d
Javier Alaves <jayalaves@gmail.com>
2023-09-19 10:08:16 +0200

    appdelegate code adjustments

--- Pachanga/PachangaApp.swift
+++ Pachanga/PachangaApp.swift
@@ -9,38 +9,83 @@ import Firebase
 import FirebaseMessaging
 import SwiftUI
 
-class AppDelegate: NSObject, UIApplicationDelegate, UNUserNotificationCenterDelegate {
-    func application(_ application: UIApplication,
-                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
-        print("Pachanga application is starting up. ApplicationDelegate didFinishLaunchingWithOptions.")
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions
+        launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        
+        // configure app to work with firebase
         FirebaseApp.configure()
-        requestPushNotificationsAuth(application: application)
+        
+        // set AppDelegate as delegate for UNUserNotificationCenter
+        UNUserNotificationCenter.current().delegate = self
+        
+        // create options related to type of push notification permissions app will request
+        let authOptions: UNAuthorizationOptions = [.alert, .badge, .sound]
+        UNUserNotificationCenter.current().requestAuthorization(
+          options: authOptions,
+          completionHandler: { _, _ in }
+        )
+        
+        // register app for remote notifications
+        application.registerForRemoteNotifications()
+        
+        // set AppDelegate as delegate for messaging
+        Messaging.messaging().delegate = self
+        
         return true
     }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
     
-    // handles how notifications will be presented
-    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
-        completionHandler([.banner, .sound])
+    // method for when notifications arrive
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        willPresent notification: UNNotification,
+        withCompletionHandler completionHandler:
+        @escaping (UNNotificationPresentationOptions) -> Void
+    ) {
+        completionHandler([[.banner, .sound]])
     }
     
-    // handles receiving notifications
-    func userNotificationCenter(_ center: UNUserNotificationCenter, didReceive response: UNNotificationResponse, withCompletionHandler completionHandler: @escaping () -> Void) {
+    // method for interacting with notifications
+    func userNotificationCenter(
+        _ center: UNUserNotificationCenter,
+        didReceive response: UNNotificationResponse,
+        withCompletionHandler completionHandler: @escaping () -> Void
+    ) {
         completionHandler()
     }
     
-    // generate request for push notifications
-    private func requestPushNotificationsAuth(application: UIApplication) {
-        UNUserNotificationCenter.current().delegate = self
-        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .badge, .sound]) { _, _ in }
-        application.registerForRemoteNotifications()
+    // generate and register a token when a user grants permission for push notifications
+    func application(
+      _ application: UIApplication,
+      didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+    ) {
+      Messaging.messaging().apnsToken = deviceToken
     }
-    
-    // assign device token to firebase cloud messaging service
-    
-    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        Messaging.messaging().apnsToken = deviceToken
-    }
-    
+
+}
+
+extension AppDelegate: MessagingDelegate {
+  // [START refresh_token]
+  func messaging(_ messaging: Messaging, didReceiveRegistrationToken fcmToken: String?) {
+    print("Firebase registration token: \(String(describing: fcmToken))")
+
+    let dataDict: [String: String] = ["token": fcmToken ?? ""]
+    NotificationCenter.default.post(
+      name: Notification.Name("FCMToken"),
+      object: nil,
+      userInfo: dataDict
+    )
+    // TODO: If necessary send token to application server.
+    // Note: This callback is fired at each app startup and whenever a new token is generated.
+  }
+
+  // [END refresh_token]
 }
 
 @main
